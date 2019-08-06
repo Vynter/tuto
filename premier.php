dd95e094d2701d5dd52a43c4f84a1092685a6d52
Vynter <amine-cheraitia@hotmail.com>
2019-08-06 23:28:06 +0100

    ajout de gitigniore

---
+++ premier.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<HTML>
+    <head>
+        <title>zzz</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    <div>
+        <h1>La liste est :</h1>
+        <ul>
+            <li style="color:red">numéro 1</li>
+            <li style="color:pink">number 1</li>
+            <li style="color:orange">zz 3</li>
+            <li style="color:green">ra9mla 4</li>
+            
+            <?php echo 'aze';
+            $age=8;
+            echo '<br>';
+            switch($age)
+            {
+                case 1: echo" c'est un";
+                case 10: echo'dix';
+                case 20: echo'vingt';
+                case 30: echo 'trente';
+                default: echo 'aucune ';
+            }
+            
+            ?>
+        </ul>
+    </div>
+        
+    </body>
+
+ </html>
